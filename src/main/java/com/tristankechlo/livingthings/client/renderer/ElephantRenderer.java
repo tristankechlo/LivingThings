24d3a1e8e34a8495980875a91cd8662d8ac1eae2
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2020-07-22 12:54:42 +0200

    Elephant Test

--- src/main/java/com/tristankechlo/livingthings/client/renderer/ElephantRenderer.java
+++ src/main/java/com/tristankechlo/livingthings/client/renderer/ElephantRenderer.java
@@ -9,7 +9,7 @@ import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.util.ResourceLocation;
 
 public class ElephantRenderer extends MobRenderer<ElephantEntity, ElephantModel<ElephantEntity>> {
-	protected static final ResourceLocation TEXTURE = new ResourceLocation(LivingThings.MOD_ID,	"textures/entity/elephant_entity.png");
+	protected static final ResourceLocation TEXTURE = new ResourceLocation(LivingThings.MOD_ID,	"textures/entity/elephant/elephant_entity.png");
 
 	public ElephantRenderer(EntityRendererManager renderManagerIn) {
 		super(renderManagerIn, new ElephantModel<>(), 1.2F);
