7b548b6383736cbee498ebfde8c5e706b512c3e2
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2020-08-27 18:51:04 +0200

    biome test

--- src/main/java/com/tristankechlo/livingthings/util/RegistryEvents.java
+++ src/main/java/com/tristankechlo/livingthings/util/RegistryEvents.java
@@ -3,8 +3,12 @@ package com.tristankechlo.livingthings.util;
 import com.google.common.base.Preconditions;
 import com.tristankechlo.livingthings.LivingThings;
 import com.tristankechlo.livingthings.init.ModEntityTypes;
+
+import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
 import net.minecraft.item.Item;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.Biome.Category;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -23,10 +27,24 @@ public class RegistryEvents {
 	}
 
     @SubscribeEvent
-    public static void registerSpawnEggs(RegistryEvent.Register<Item> event) {
+    public static void onItemRegistry(RegistryEvent.Register<Item> event) {
         for (Item spawnEgg : ModEntityTypes.SPAWN_EGGS) {
             Preconditions.checkNotNull(spawnEgg.getRegistryName(), "registryName");
             event.getRegistry().register(spawnEgg);
         }
     }
+    
+    @SubscribeEvent
+    public static void onBiomeRegistry(RegistryEvent.Register<Biome> event) {
+    	for (Biome biome : event.getRegistry().getValues()) {
+    		if(biome.getCategory() == Category.SAVANNA) {
+        		biome.func_242433_b().func_242559_a(EntityClassification.CREATURE);
+    		}
+    	}
+    }
 }
+/*
+ * .add(new Spawners(ModEntityTypes.ELEPHANT_ENTITY, 15, 2, 5))
+ * .add(new Spawners(ModEntityTypes.GIRAFFE_ENTITY, 15, 2, 4))
+ * .add(new Spawners(ModEntityTypes.LION_ENTITY, 15, 3, 5))
+ */
