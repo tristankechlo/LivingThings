11a3406dd7b40c689affdb45a551e30099c795d9
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2020-09-28 16:29:46 +0200

    Update MantarayModel.java

--- src/main/java/com/tristankechlo/livingthings/client/model/MantarayModel.java
+++ src/main/java/com/tristankechlo/livingthings/client/model/MantarayModel.java
@@ -42,16 +42,18 @@ public class MantarayModel<T extends MantarayEntity> extends EntityModel<T> {
 		Tail2 = new ModelRenderer(this);
 		Tail2.setRotationPoint(0.0F, -0.25F, 6.0F);
 		Tail.addChild(Tail2);
-		Tail2.setTextureOffset(33, 42).addBox(-0.5F, -1.5F, 0.0F, 1.0F, 1.0F, 6.0F, 0.0F, false);
+		Tail2.setTextureOffset(33, 41).addBox(-0.5F, -1.5F, 0.0F, 1.0F, 1.0F, 7.0F, 0.0F, false);
 
 		LeftFlipper = new ModelRenderer(this);
 		LeftFlipper.setRotationPoint(3.5F, -2.5F, -0.25F);
 		Body.addChild(LeftFlipper);
+		setRotationAngle(LeftFlipper, 0.0F, 0.0F, -0.0873F);
 		LeftFlipper.setTextureOffset(0, 1).addBox(0.0F, -1.0F, -4.5F, 4.0F, 2.0F, 9.0F, 0.0F, false);
 
 		LeftFlipper2 = new ModelRenderer(this);
 		LeftFlipper2.setRotationPoint(4.0F, 0.0F, 0.0F);
 		LeftFlipper.addChild(LeftFlipper2);
+		setRotationAngle(LeftFlipper2, 0.0F, 0.0F, -0.0873F);
 		LeftFlipper2.setTextureOffset(27, 3).addBox(0.0F, -1.0F, -4.25F, 2.0F, 1.0F, 8.0F, 0.0F, false);
 		LeftFlipper2.setTextureOffset(0, 13).addBox(0.0F, -0.5F, -4.25F, 2.0F, 1.0F, 8.0F, 0.0F, false);
 		LeftFlipper2.setTextureOffset(0, 23).addBox(2.0F, -1.0F, -4.0F, 3.0F, 1.0F, 7.0F, 0.0F, false);
@@ -63,11 +65,13 @@ public class MantarayModel<T extends MantarayEntity> extends EntityModel<T> {
 		RightFlipper = new ModelRenderer(this);
 		RightFlipper.setRotationPoint(-3.5F, -2.5F, -0.25F);
 		Body.addChild(RightFlipper);
+		setRotationAngle(RightFlipper, 0.0F, 0.0F, 0.0873F);
 		RightFlipper.setTextureOffset(0, 1).addBox(-4.0F, -1.0F, -4.5F, 4.0F, 2.0F, 9.0F, 0.0F, true);
 
 		RightFlipper2 = new ModelRenderer(this);
 		RightFlipper2.setRotationPoint(-4.0F, 0.0F, 0.0F);
 		RightFlipper.addChild(RightFlipper2);
+		setRotationAngle(RightFlipper2, 0.0F, 0.0F, 0.0873F);
 		RightFlipper2.setTextureOffset(27, 3).addBox(-2.0F, -1.0F, -4.25F, 2.0F, 1.0F, 8.0F, 0.0F, true);
 		RightFlipper2.setTextureOffset(0, 13).addBox(-2.0F, -0.5F, -4.25F, 2.0F, 1.0F, 8.0F, 0.0F, true);
 		RightFlipper2.setTextureOffset(0, 23).addBox(-5.0F, -1.0F, -4.0F, 3.0F, 1.0F, 7.0F, 0.0F, true);
@@ -94,21 +98,27 @@ public class MantarayModel<T extends MantarayEntity> extends EntityModel<T> {
 
 	@Override
 	public void setRotationAngles(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch){
-		this.Body.rotateAngleX = headPitch * ((float) Math.PI / 180F);
-		this.Body.rotateAngleY = netHeadYaw * ((float) Math.PI / 180F);
-		
-		this.Tail.rotateAngleX = -0.5F * headPitch * ((float) Math.PI / 180F);
-		this.Tail2.rotateAngleX = -0.25F * headPitch * ((float) Math.PI / 180F);
-		
-	      if (Entity.horizontalMag(entity.getMotion()) > 1.0E-7D) {
-	          this.Body.rotateAngleX += -0.05F + (-0.05F * MathHelper.cos(ageInTicks * 0.2F));
-	          
-	          //wings flapping
-	          this.LeftFlipper.rotateAngleZ = -0.6F * MathHelper.cos(ageInTicks * 0.15F);
-	          this.LeftFlipper2.rotateAngleZ = -0.6F * MathHelper.cos(ageInTicks * 0.15F);
-	          this.RightFlipper.rotateAngleZ = 0.6F * MathHelper.cos(ageInTicks * 0.15F);
-	          this.RightFlipper2.rotateAngleZ = 0.6F * MathHelper.cos(ageInTicks * 0.15F);
-	       }
+		this.Body.rotateAngleX = headPitch * 0.0174532925F;
+		this.Body.rotateAngleY = netHeadYaw * 0.0174532925F;
+
+		this.Tail.rotateAngleX = -0.5F * headPitch * 0.0174532925F;
+		this.Tail2.rotateAngleX = -0.25F * headPitch * 0.0174532925F;
+
+		// needed, so that the patchouli entry is always the same
+		this.LeftFlipper.rotateAngleZ = -0.34906585F;
+		this.LeftFlipper2.rotateAngleZ = -0.34906585F;
+		this.RightFlipper.rotateAngleZ = 0.34906585F;
+		this.RightFlipper2.rotateAngleZ = 0.34906585F;
+
+		if (Entity.horizontalMag(entity.getMotion()) > 1.0E-7D) {
+			this.Body.rotateAngleX += -0.05F + (-0.05F * MathHelper.cos(ageInTicks * 0.2F));
+
+			// wings flapping
+			this.LeftFlipper.rotateAngleZ = -0.6F * MathHelper.cos(ageInTicks * 0.15F);
+			this.LeftFlipper2.rotateAngleZ = -0.6F * MathHelper.cos(ageInTicks * 0.15F);
+			this.RightFlipper.rotateAngleZ = 0.6F * MathHelper.cos(ageInTicks * 0.15F);
+			this.RightFlipper2.rotateAngleZ = 0.6F * MathHelper.cos(ageInTicks * 0.15F);
+		}
 	}
 
 	@Override
