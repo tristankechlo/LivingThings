5f82e5638a4f7ae64e3d4032dfda2f1cccbab226
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2020-09-29 12:15:06 +0200

    item group now sorted
    spawn eggs are always in the end of the list now

--- src/main/java/com/tristankechlo/livingthings/init/ModItemGroups.java
+++ src/main/java/com/tristankechlo/livingthings/init/ModItemGroups.java
@@ -1,14 +1,37 @@
 package com.tristankechlo.livingthings.init;
 
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.SpawnEggItem;
+import net.minecraft.util.NonNullList;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class ModItemGroups {
-	
-    public static final ItemGroup General = new ItemGroup("LivingThings") {
+
+	public static final ItemGroup General = new ItemGroup("LivingThings") {
+		
 		@Override
 		public ItemStack createIcon() {
 			return new ItemStack(ModItems.SHARK_TOOTH.get());
+		}
+
+		@Override
+		@OnlyIn(Dist.CLIENT)
+		public void fill(NonNullList<ItemStack> items) {
+			NonNullList<ItemStack> spawneggsItems = NonNullList.create();
+			for (Item item : ForgeRegistries.ITEMS) {
+				if(!(item instanceof SpawnEggItem)) {
+					item.fillItemGroup(this, items);					
+				} else {
+					if (item.getGroup() == this) {
+						spawneggsItems.add(new ItemStack(item));
+					}
+				}
+			}
+			items.addAll(spawneggsItems);
 		}
 	};
 
