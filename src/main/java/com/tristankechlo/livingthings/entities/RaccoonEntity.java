52121d33a3b2e321099e9ccbfce840e4c69a2954
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2020-10-05 20:18:00 +0200

    Update RaccoonEntity.java

--- src/main/java/com/tristankechlo/livingthings/entities/RaccoonEntity.java
+++ src/main/java/com/tristankechlo/livingthings/entities/RaccoonEntity.java
@@ -55,18 +55,18 @@ public class RaccoonEntity extends AnimalEntity implements IAngerable {
 	public static AttributeModifierMap.MutableAttribute getAttributes() {
 		return MobEntity.func_233666_p_()
 				.createMutableAttribute(Attributes.MAX_HEALTH, LivingThingsConfig.RACCOON.health.get())
-				.createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.2D)
+				.createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.23D)
 				.createMutableAttribute(Attributes.ATTACK_DAMAGE, LivingThingsConfig.RACCOON.damage.get());
 	}
 	
 	@Override
 	protected void registerGoals() {
 		this.goalSelector.addGoal(0, new SwimGoal(this));
-		this.goalSelector.addGoal(1, new BetterMeleeAttackGoal(this, 1.05D, false));
+		this.goalSelector.addGoal(1, new BetterMeleeAttackGoal(this, 1.25D, false));
 		this.goalSelector.addGoal(2, new BreedGoal(this, 1.0D));
-		this.goalSelector.addGoal(3, new TemptGoal(this, 1.05D, false, BREEDING_ITEMS));
-		this.goalSelector.addGoal(4, new FollowParentGoal(this, 0.95D));
-		this.goalSelector.addGoal(5, new WaterAvoidingRandomWalkingGoal(this, 0.9D));
+		this.goalSelector.addGoal(3, new TemptGoal(this, 1.1D, false, BREEDING_ITEMS));
+		this.goalSelector.addGoal(4, new FollowParentGoal(this, 1.1D));
+		this.goalSelector.addGoal(5, new WaterAvoidingRandomWalkingGoal(this, 1.0D));
 		this.goalSelector.addGoal(6, new LookAtGoal(this, PlayerEntity.class, 8.0F));
 		this.goalSelector.addGoal(7, new LookRandomlyGoal(this));
 
@@ -110,7 +110,7 @@ public class RaccoonEntity extends AnimalEntity implements IAngerable {
 	
 	@Override
 	public int getMaxSpawnedInChunk() {
-		return 6;
+		return LivingThingsConfig.RACCOON.maxSpawns.get();
 	}
 	
 	@Override
