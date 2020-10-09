17fa13b8cedad4ef5f72066229c08f4d9803e66f
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2020-10-09 13:26:10 +0200

    improve model and add animations

--- src/main/java/com/tristankechlo/livingthings/client/model/OwlModel.java
+++ src/main/java/com/tristankechlo/livingthings/client/model/OwlModel.java
@@ -6,15 +6,17 @@ import com.tristankechlo.livingthings.entities.OwlEntity;
 
 import net.minecraft.client.renderer.entity.model.EntityModel;
 import net.minecraft.client.renderer.model.ModelRenderer;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
-public class OwlModel <T extends OwlEntity> extends EntityModel<T> {
+public class OwlModel<T extends OwlEntity> extends EntityModel<T> {
 
 	private final ModelRenderer Body;
-	private final ModelRenderer Tail;
-	private final ModelRenderer Legs;
+	private final ModelRenderer LeftWing;
+	private final ModelRenderer RightWing;
 	private final ModelRenderer LeftLeg;
 	private final ModelRenderer LeftFoot;
 	private final ModelRenderer RightToeLeftFoot;
@@ -23,11 +25,9 @@ public class OwlModel <T extends OwlEntity> extends EntityModel<T> {
 	private final ModelRenderer RightFoot;
 	private final ModelRenderer RightToeRightFoot;
 	private final ModelRenderer LeftToeRightFoot;
+	private final ModelRenderer Tail;
 	private final ModelRenderer Head;
 	private final ModelRenderer Beak;
-	private final ModelRenderer Fings;
-	private final ModelRenderer LeftWing;
-	private final ModelRenderer RightWing;
 
 	public OwlModel() {
 		textureWidth = 64;
@@ -38,21 +38,22 @@ public class OwlModel <T extends OwlEntity> extends EntityModel<T> {
 		setRotationAngle(Body, 0.1745F, 0.0F, 0.0F);
 		Body.setTextureOffset(0, 17).addBox(-3.5F, -9.025F, -3.5F, 7.0F, 9.0F, 6.0F, 0.0F, false);
 
-		Tail = new ModelRenderer(this);
-		Tail.setRotationPoint(0.0F, -0.7652F, 1.3264F);
-		Body.addChild(Tail);
-		setRotationAngle(Tail, -0.9599F, 0.0F, 0.0F);
-		Tail.setTextureOffset(31, 13).addBox(-1.0F, -0.5186F, -0.2595F, 2.0F, 1.0F, 3.0F, 0.0F, false);
+		LeftWing = new ModelRenderer(this);
+		LeftWing.setRotationPoint(3.5F, -8.5792F, -0.4056F);
+		Body.addChild(LeftWing);
+		setRotationAngle(LeftWing, 0.0F, 0.0F, -0.1309F);
+		LeftWing.setTextureOffset(0, 1).addBox(0.0F, 0.0F, -3.0F, 1.0F, 8.0F, 6.0F, 0.0F, false);
 
-		Legs = new ModelRenderer(this);
-		Legs.setRotationPoint(0.0F, -1.9107F, -1.5699F);
-		Body.addChild(Legs);
-		setRotationAngle(Legs, -0.2618F, 0.0F, 0.0F);
-		
+		RightWing = new ModelRenderer(this);
+		RightWing.setRotationPoint(-3.5F, -8.5792F, -0.4056F);
+		Body.addChild(RightWing);
+		setRotationAngle(RightWing, 0.0F, 0.0F, 0.1309F);
+		RightWing.setTextureOffset(15, 1).addBox(-1.0F, 0.0F, -3.0F, 1.0F, 8.0F, 6.0F, 0.0F, false);
 
 		LeftLeg = new ModelRenderer(this);
-		LeftLeg.setRotationPoint(2.0F, 0.0F, 0.0F);
-		Legs.addChild(LeftLeg);
+		LeftLeg.setRotationPoint(2.0F, -1.9107F, -1.5699F);
+		Body.addChild(LeftLeg);
+		setRotationAngle(LeftLeg, -0.2618F, 0.0F, 0.0F);
 		LeftLeg.setTextureOffset(31, 1).addBox(-1.0F, 0.1224F, -0.5671F, 2.0F, 3.0F, 2.0F, 0.0F, false);
 
 		LeftFoot = new ModelRenderer(this);
@@ -74,8 +75,9 @@ public class OwlModel <T extends OwlEntity> extends EntityModel<T> {
 		LeftToeLeftFoot.setTextureOffset(54, 4).addBox(-0.5F, -0.5F, -0.925F, 1.0F, 1.0F, 1.0F, 0.0F, false);
 
 		RightLeg = new ModelRenderer(this);
-		RightLeg.setRotationPoint(-2.0F, 0.0F, 0.0F);
-		Legs.addChild(RightLeg);
+		RightLeg.setRotationPoint(-2.0F, -1.9107F, -1.5699F);
+		Body.addChild(RightLeg);
+		setRotationAngle(RightLeg, -0.2618F, 0.0F, 0.0F);
 		RightLeg.setTextureOffset(31, 7).addBox(-1.0F, 0.1224F, -0.5671F, 2.0F, 3.0F, 2.0F, 0.0F, false);
 
 		RightFoot = new ModelRenderer(this);
@@ -96,6 +98,12 @@ public class OwlModel <T extends OwlEntity> extends EntityModel<T> {
 		setRotationAngle(LeftToeRightFoot, 0.0F, -0.1745F, 0.0F);
 		LeftToeRightFoot.setTextureOffset(54, 10).addBox(-0.5F, -0.5F, -0.925F, 1.0F, 1.0F, 1.0F, 0.0F, false);
 
+		Tail = new ModelRenderer(this);
+		Tail.setRotationPoint(0.0F, -0.7652F, 1.3264F);
+		Body.addChild(Tail);
+		setRotationAngle(Tail, -0.9599F, 0.0F, 0.0F);
+		Tail.setTextureOffset(31, 13).addBox(-1.0F, -0.5186F, -0.2595F, 2.0F, 1.0F, 3.0F, 0.0F, false);
+
 		Head = new ModelRenderer(this);
 		Head.setRotationPoint(0.0F, -8.5045F, -0.1394F);
 		Body.addChild(Head);
@@ -107,37 +115,102 @@ public class OwlModel <T extends OwlEntity> extends EntityModel<T> {
 		Head.addChild(Beak);
 		setRotationAngle(Beak, 0.4363F, 0.0F, 0.0F);
 		Beak.setTextureOffset(42, 14).addBox(-0.5F, -0.5F, -2.0F, 1.0F, 1.0F, 2.0F, 0.0F, false);
-
-		Fings = new ModelRenderer(this);
-		Fings.setRotationPoint(0.0F, -8.0F, 0.0F);
-		Body.addChild(Fings);
-		
-
-		LeftWing = new ModelRenderer(this);
-		LeftWing.setRotationPoint(3.5F, -0.5792F, -0.4056F);
-		Fings.addChild(LeftWing);
-		setRotationAngle(LeftWing, 0.0F, 0.0F, -0.1309F);
-		LeftWing.setTextureOffset(0, 1).addBox(0.0F, 0.0F, -3.0F, 1.0F, 8.0F, 6.0F, 0.0F, false);
-
-		RightWing = new ModelRenderer(this);
-		RightWing.setRotationPoint(-3.5F, -0.5792F, -0.4056F);
-		Fings.addChild(RightWing);
-		setRotationAngle(RightWing, 0.0F, 0.0F, 0.1309F);
-		RightWing.setTextureOffset(15, 1).addBox(-1.0F, 0.0F, -3.0F, 1.0F, 8.0F, 6.0F, 0.0F, false);
 	}
 
 	@Override
-	public void setRotationAngles(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch){
-		//previously the render function, render code was moved to a method below
-	}
-
-	@Override
-	public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha){
-		if(this.isChild) {
+	public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red,
+			float green, float blue, float alpha) {
+		if (this.isChild) {
 			matrixStack.scale(0.5F, 0.5F, 0.5F);
 			matrixStack.translate(0, 1.5D, 0);
 		}
 		Body.render(matrixStack, buffer, packedLight, packedOverlay);
+	}
+
+	@Override
+	public void setRotationAngles(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw,	float headPitch) {
+		this.setRotationAngles(getOwlState(entity), entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw,	headPitch);
+	}
+
+	@Override
+	public void setLivingAnimations(T entity, float limbSwing, float limbSwingAmount, float partialTick) {
+		this.setLivingAnimations(getOwlState(entity), entity);
+	}
+
+	public void setRotationAngles(OwlState state, T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+		this.Head.rotateAngleX = -0.174532F + headPitch * ((float) Math.PI / 180F);
+		this.Head.rotateAngleY = netHeadYaw * ((float) Math.PI / 180F);
+	    this.Head.rotateAngleZ = 0.0F;
+	    this.Head.rotationPointX = 0.0F;
+	    this.Body.rotationPointX = 0.0F;
+	    this.Tail.rotationPointX = 0.0F;
+	    this.RightWing.rotationPointX = -3.5F;
+	    this.LeftWing.rotationPointX = 3.5F;
+		switch (state) {
+			case SITTING:
+				break;				
+			case STANDING:
+		         this.LeftLeg.rotateAngleX += MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
+		         this.RightLeg.rotateAngleX += MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
+				break;				
+			case FLYING:
+			default:
+		         this.Tail.rotateAngleX = -0.959931F + MathHelper.cos(limbSwing * 0.6662F) * 0.3F * limbSwingAmount;
+		         this.LeftWing.rotateAngleZ = -0.0873F - ageInTicks;
+		         this.RightWing.rotateAngleZ = 0.0873F + ageInTicks;
+		         break;
+		}
+
+	}
+
+	public void setLivingAnimations(OwlState state, OwlEntity owl) {
+	    this.Body.rotateAngleX = 0.174532F;
+	    
+	    this.LeftWing.rotateAngleX = 0F;
+	    this.LeftWing.rotateAngleY = 0F;
+	    this.LeftWing.rotateAngleZ = -0.130899F;
+	    
+	    this.RightWing.rotateAngleX = 0F;
+	    this.RightWing.rotateAngleY = 0F;
+	    this.RightWing.rotateAngleZ = 0.130899F;
+	    
+	    this.LeftLeg.rotateAngleX = -0.261799F;
+	    this.LeftLeg.rotationPointY = -1.9107F;
+	    this.LeftLeg.rotateAngleZ = 0F;
+	    
+	    this.RightLeg.rotateAngleX = -0.261799F;
+	    this.RightLeg.rotationPointY = -1.9107F;
+	    this.RightLeg.rotateAngleZ = 0F;	    
+	    
+		switch (state) {
+			case SITTING:
+				this.Body.rotateAngleX = 0F;
+			    this.LeftLeg.rotateAngleX = 0F;
+			    this.RightLeg.rotateAngleX = 0F;
+				break;
+			case FLYING:
+				if (Entity.horizontalMag(owl.getMotion()) > 1.0E-7D) {
+			        this.LeftLeg.rotateAngleX += 0.6981317F;
+			        this.RightLeg.rotateAngleX += 0.6981317F;
+				}
+				break;
+			case STANDING:	
+			default:
+				break;
+		}
+	}
+
+	private static OwlState getOwlState(OwlEntity owl) {
+		if (owl.isSitting() || owl.isSleeping()) {
+			return OwlState.SITTING;
+		} else {
+			return owl.isFlying() ? OwlState.FLYING : OwlState.STANDING;
+		}
+	}
+
+	@OnlyIn(Dist.CLIENT)
+	public static enum OwlState {
+		FLYING, STANDING, SITTING;
 	}
 
 	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
--- src/main/java/com/tristankechlo/livingthings/client/renderer/OwlRenderer.java
+++ src/main/java/com/tristankechlo/livingthings/client/renderer/OwlRenderer.java
@@ -1,5 +1,6 @@
 package com.tristankechlo.livingthings.client.renderer;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
 import com.tristankechlo.livingthings.LivingThings;
 import com.tristankechlo.livingthings.client.model.OwlModel;
 import com.tristankechlo.livingthings.entities.OwlEntity;
@@ -7,6 +8,7 @@ import com.tristankechlo.livingthings.entities.OwlEntity;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
@@ -18,7 +20,7 @@ public class OwlRenderer extends MobRenderer<OwlEntity, OwlModel<OwlEntity>>{
 	protected static final ResourceLocation TEXTURE_BLACK = new ResourceLocation(LivingThings.MOD_ID, "textures/entity/owl/owl_black.png");
 
 	public OwlRenderer(EntityRendererManager renderManagerIn) {
-		super(renderManagerIn, new OwlModel<>(), 0.35F);
+		super(renderManagerIn, new OwlModel<>(), 0.29F);
 	}
 
 	@Override
@@ -30,6 +32,19 @@ public class OwlRenderer extends MobRenderer<OwlEntity, OwlModel<OwlEntity>>{
 			return TEXTURE_BLACK;
 		}
 		return TEXTURE_BROWN;
+	}
+	
+	@Override
+	protected void preRenderCallback(OwlEntity entitylivingbaseIn, MatrixStack matrixStackIn, float partialTickTime) {
+		//scale the owl down a bit, to be smaller than one block
+		matrixStackIn.scale(0.94F, 0.94F, 0.94F);
+	}
+	
+	@Override
+	protected float handleRotationFloat(OwlEntity livingBase, float partialTicks) {
+		float f = MathHelper.lerp(partialTicks, livingBase.oFlap, livingBase.flap);
+		float f1 = MathHelper.lerp(partialTicks, livingBase.oFlapSpeed, livingBase.flapSpeed);
+		return (MathHelper.sin(f) + 1.0F) * f1;
 	}
 
 }
