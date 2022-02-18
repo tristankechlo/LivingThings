22a9a5408f3b4f8a986f03fa25fc25717a3a3f4a
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2022-02-18 20:53:09 +0100

    update elephant model and texture

--- src/main/java/com/tristankechlo/livingthings/client/model/entity/ElephantModel.java
+++ src/main/java/com/tristankechlo/livingthings/client/model/entity/ElephantModel.java
@@ -33,30 +33,27 @@ public class ElephantModel<T extends ElephantEntity> extends AdvancedEntityModel
 	private final ModelPart TrunkTop;
 	private final ModelPart TrunkMiddle;
 	private final ModelPart TrunkBottom;
-	private final ModelPart Back;
-	private final ModelPart Front;
-	private final ModelPart Legs;
 	private final ModelPart RightFrontLeg;
 	private final ModelPart LeftFrontLeg;
 	private final ModelPart RightBackLeg;
 	private final ModelPart LeftBackLeg;
 
 	public ElephantModel(ModelPart root) {
-		this.Tusks = root.getChild("Tusks");
 		this.Body = root.getChild("Body");
-		this.Chests = root.getChild("Chests");
-		this.Saddle = root.getChild("Saddle");
+
+		this.Chests = Body.getChild("Chests");
+		this.Saddle = Body.getChild("Saddle");
 		this.Head = Body.getChild("Head");
+
+		this.Tusks = Head.getChild("Tusks");
 		this.TrunkTop = Head.getChild("TrunkTop");
 		this.TrunkMiddle = TrunkTop.getChild("TrunkMiddle");
 		this.TrunkBottom = TrunkMiddle.getChild("TrunkBottom");
-		this.Legs = Body.getChild("Legs");
-		this.Front = Legs.getChild("Front");
-		this.Back = Legs.getChild("Back");
-		this.RightFrontLeg = Front.getChild("RightFrontLeg");
-		this.LeftFrontLeg = Front.getChild("LeftFrontLeg");
-		this.RightBackLeg = Back.getChild("RightBackLeg");
-		this.LeftBackLeg = Back.getChild("LeftBackLeg");
+
+		this.RightFrontLeg = Body.getChild("RightFrontLeg");
+		this.LeftFrontLeg = Body.getChild("LeftFrontLeg");
+		this.RightBackLeg = Body.getChild("RightBackLeg");
+		this.LeftBackLeg = Body.getChild("LeftBackLeg");
 	}
 
 	@SuppressWarnings("unused")
@@ -64,74 +61,11 @@ public class ElephantModel<T extends ElephantEntity> extends AdvancedEntityModel
 		MeshDefinition meshdefinition = new MeshDefinition();
 		PartDefinition partdefinition = meshdefinition.getRoot();
 
-		PartDefinition Tusks = partdefinition.addOrReplaceChild("Tusks", CubeListBuilder.create(),
-				PartPose.offset(0.0F, -12.0F, -19.0F));
-
-		PartDefinition LeftTusk = Tusks.addOrReplaceChild("LeftTusk", CubeListBuilder.create(),
-				PartPose.offset(7.0F, 6.0F, -10.0F));
-
-		PartDefinition LeftTuskTop = LeftTusk.addOrReplaceChild("LeftTuskTop", CubeListBuilder.create().texOffs(192, 0)
-				.addBox(-1.0F, 0.0F, -1.0F, 2.0F, 10.0F, 2.0F, new CubeDeformation(0.0F)),
-				PartPose.offset(0.0F, 0.0F, 0.0F));
-
-		PartDefinition LeftTuskMiddle = LeftTuskTop.addOrReplaceChild("LeftTuskMiddle",
-				CubeListBuilder.create().texOffs(204, 0).addBox(-1.0F, 0.0F, -1.0F, 2.0F, 6.0F, 2.0F,
-						new CubeDeformation(0.0F)),
-				PartPose.offsetAndRotation(0.0F, 10.0F, 0.0F, -0.3054F, 0.0F, 0.0F));
-
-		PartDefinition LeftTuskBottom = LeftTuskMiddle
-				.addOrReplaceChild("LeftTuskBottom",
-						CubeListBuilder.create().texOffs(215, 0).addBox(-1.0F, 0.0F, -1.0F, 2.0F, 5.0F, 2.0F,
-								new CubeDeformation(0.0F)),
-						PartPose.offsetAndRotation(0.0F, 6.0F, 0.0F, -0.6545F, 0.0F, 0.0F));
-
-		PartDefinition RightTusk = Tusks.addOrReplaceChild("RightTusk", CubeListBuilder.create(),
-				PartPose.offset(-7.0F, 6.0F, -10.0F));
-
-		PartDefinition RightTuskTop = RightTusk.addOrReplaceChild("RightTuskTop", CubeListBuilder.create()
-				.texOffs(228, 0).addBox(-15.0F, 0.0F, -1.0F, 2.0F, 10.0F, 2.0F, new CubeDeformation(0.0F)),
-				PartPose.offset(14.0F, 0.0F, 0.0F));
-
-		PartDefinition RightTuskMiddle = RightTuskTop.addOrReplaceChild("RightTuskMiddle",
-				CubeListBuilder.create().texOffs(238, 0).addBox(-15.0F, 0.0F, -1.0F, 2.0F, 6.0F, 2.0F,
-						new CubeDeformation(0.0F)),
-				PartPose.offsetAndRotation(0.0F, 10.0F, 0.0F, -0.3054F, 0.0F, 0.0F));
-
-		PartDefinition RightTuskBottom = RightTuskMiddle.addOrReplaceChild(
-				"RightTuskBottom", CubeListBuilder.create().texOffs(248, 0).addBox(-15.0F, 0.0F, -1.0F, 2.0F, 5.0F,
-						2.0F, new CubeDeformation(0.0F)),
-				PartPose.offsetAndRotation(0.0F, 6.0F, 0.0F, -0.6545F, 0.0F, 0.0F));
-
 		PartDefinition Body = partdefinition.addOrReplaceChild("Body",
 				CubeListBuilder.create().texOffs(0, 64)
 						.addBox(-11.0F, -42.0F, -20.0F, 22.0F, 24.0F, 40.0F, new CubeDeformation(0.0F)).texOffs(156, 93)
 						.addBox(-8.0F, -43.0F, -17.0F, 16.0F, 1.0F, 34.0F, new CubeDeformation(0.0F)),
 				PartPose.offset(0.0F, 24.0F, 0.0F));
-
-		PartDefinition Legs = Body.addOrReplaceChild("Legs", CubeListBuilder.create(),
-				PartPose.offset(0.0F, 0.0F, 0.0F));
-
-		PartDefinition Front = Legs.addOrReplaceChild("Front", CubeListBuilder.create(),
-				PartPose.offset(0.0F, 0.0F, 0.0F));
-
-		PartDefinition RightFrontLeg = Front.addOrReplaceChild("RightFrontLeg", CubeListBuilder.create().texOffs(68, 0)
-				.addBox(-3.0F, 1.0F, -3.0F, 6.0F, 18.0F, 6.0F, new CubeDeformation(0.0F)),
-				PartPose.offset(-8.0F, -19.0F, -16.0F));
-
-		PartDefinition LeftFrontLeg = Front.addOrReplaceChild("LeftFrontLeg", CubeListBuilder.create().texOffs(95, 0)
-				.addBox(-3.0F, 1.0F, -3.0F, 6.0F, 18.0F, 6.0F, new CubeDeformation(0.0F)),
-				PartPose.offset(8.0F, -19.0F, -16.0F));
-
-		PartDefinition Back = Legs.addOrReplaceChild("Back", CubeListBuilder.create(),
-				PartPose.offset(0.0F, 0.0F, 0.0F));
-
-		PartDefinition RightBackLeg = Back.addOrReplaceChild("RightBackLeg", CubeListBuilder.create().texOffs(122, 0)
-				.addBox(-3.0F, 1.0F, -3.0F, 6.0F, 18.0F, 6.0F, new CubeDeformation(0.0F)),
-				PartPose.offset(-8.0F, -19.0F, 16.0F));
-
-		PartDefinition LeftBackLeg = Back.addOrReplaceChild("LeftBackLeg", CubeListBuilder.create().texOffs(149, 0)
-				.addBox(-3.0F, 1.0F, -3.0F, 6.0F, 18.0F, 6.0F, new CubeDeformation(0.0F)),
-				PartPose.offset(8.0F, -19.0F, 16.0F));
 
 		PartDefinition Head = Body.addOrReplaceChild("Head",
 				CubeListBuilder.create().texOffs(123, 45).addBox(-9.0F, -9.0F, -12.0F, 18.0F, 16.0F, 13.0F,
@@ -156,58 +90,111 @@ public class ElephantModel<T extends ElephantEntity> extends AdvancedEntityModel
 								new CubeDeformation(0.0F)),
 						PartPose.offsetAndRotation(0.0F, 7.0F, 0.0F, 0.0873F, 0.0F, 0.0F));
 
-		PartDefinition Ears = Head.addOrReplaceChild("Ears", CubeListBuilder.create(),
-				PartPose.offset(0.0F, 0.0F, 0.0F));
-
-		PartDefinition LeftEar = Ears.addOrReplaceChild("LeftEar",
-				CubeListBuilder.create().texOffs(200, 37).addBox(0.0F, -8.0F, 0.0F, 1.0F, 12.0F, 8.0F,
-						new CubeDeformation(0.0F)),
+		PartDefinition LeftEar = Head.addOrReplaceChild("LeftEar",
+				CubeListBuilder.create().texOffs(220, 37).mirror()
+						.addBox(0.0F, -8.0F, 0.0F, 1.0F, 12.0F, 8.0F, new CubeDeformation(0.0F)).mirror(false),
 				PartPose.offsetAndRotation(9.0F, -1.0F, -6.0F, 0.1309F, 0.48F, 0.0F));
 
-		PartDefinition RightEar = Ears.addOrReplaceChild("RightEar",
+		PartDefinition RightEar = Head.addOrReplaceChild("RightEar",
 				CubeListBuilder.create().texOffs(200, 37).addBox(-1.0F, -8.0F, 0.0F, 1.0F, 12.0F, 8.0F,
 						new CubeDeformation(0.0F)),
 				PartPose.offsetAndRotation(-9.0F, -1.0F, -6.0F, 0.1309F, -0.48F, 0.0F));
 
-		PartDefinition Chests = partdefinition.addOrReplaceChild("Chests", CubeListBuilder.create().texOffs(54, 38)
-				.addBox(11.0F, -37.0F, 9.5F, 2.0F, 8.0F, 8.0F, new CubeDeformation(0.0F)).texOffs(54, 38).mirror()
-				.addBox(-13.0F, -37.0F, 9.5F, 2.0F, 8.0F, 8.0F, new CubeDeformation(0.0F)).mirror(false)
-				.texOffs(136, 85).addBox(10.2F, -42.475F, 11.5F, 1.0F, 6.0F, 4.0F, new CubeDeformation(0.0F))
-				.texOffs(136, 85).addBox(-11.2F, -42.475F, 11.5F, 1.0F, 6.0F, 4.0F, new CubeDeformation(0.0F))
-				.texOffs(125, 92).addBox(-8.5F, -43.475F, 11.5F, 17.0F, 1.0F, 4.0F, new CubeDeformation(0.0F)),
-				PartPose.offset(0.0F, 24.0F, 0.0F));
+		PartDefinition Tusks = Head.addOrReplaceChild("Tusks", CubeListBuilder.create(),
+				PartPose.offset(0.0F, 0.0F, 0.0F));
+
+		PartDefinition RightTusk = Tusks.addOrReplaceChild("RightTusk", CubeListBuilder.create(),
+				PartPose.offset(-7.0F, 6.0F, -9.0F));
+
+		PartDefinition RightTuskTop = RightTusk.addOrReplaceChild("RightTuskTop", CubeListBuilder.create()
+				.texOffs(192, 0).addBox(-15.0F, 0.0F, -1.0F, 2.0F, 10.0F, 2.0F, new CubeDeformation(0.0F)),
+				PartPose.offset(14.0F, 0.0F, 0.0F));
+
+		PartDefinition RightTuskMiddle = RightTuskTop.addOrReplaceChild("RightTuskMiddle",
+				CubeListBuilder.create().texOffs(204, 0).mirror()
+						.addBox(-15.0F, -0.35F, -1.05F, 2.0F, 6.0F, 2.0F, new CubeDeformation(-0.001F)).mirror(false),
+				PartPose.offsetAndRotation(0.0F, 10.0F, 0.0F, -0.3054F, 0.0F, 0.0F));
+
+		PartDefinition RightTuskBottom = RightTuskMiddle.addOrReplaceChild("RightTuskBottom",
+				CubeListBuilder.create().texOffs(215, 0).addBox(-15.0F, -0.85F, -1.475F, 2.0F, 5.0F, 2.0F,
+						new CubeDeformation(-0.002F)),
+				PartPose.offsetAndRotation(0.0F, 6.0F, 0.0F, -0.6545F, 0.0F, 0.0F));
+
+		PartDefinition LeftTusk = Tusks.addOrReplaceChild("LeftTusk", CubeListBuilder.create(),
+				PartPose.offset(7.0F, 6.0F, -9.0F));
+
+		PartDefinition LeftTuskTop = LeftTusk.addOrReplaceChild("LeftTuskTop",
+				CubeListBuilder.create().texOffs(192, 0).mirror()
+						.addBox(-1.0F, 0.0F, -1.0F, 2.0F, 10.0F, 2.0F, new CubeDeformation(0.0F)).mirror(false),
+				PartPose.offset(0.0F, 0.0F, 0.0F));
+
+		PartDefinition LeftTuskMiddle = LeftTuskTop.addOrReplaceChild("LeftTuskMiddle",
+				CubeListBuilder.create().texOffs(204, 0).addBox(-1.0F, -0.35F, -1.05F, 2.0F, 6.0F, 2.0F,
+						new CubeDeformation(-0.001F)),
+				PartPose.offsetAndRotation(0.0F, 10.0F, 0.0F, -0.3054F, 0.0F, 0.0F));
+
+		PartDefinition LeftTuskBottom = LeftTuskMiddle.addOrReplaceChild("LeftTuskBottom",
+				CubeListBuilder.create().texOffs(215, 0).mirror()
+						.addBox(-1.0F, -0.85F, -1.475F, 2.0F, 5.0F, 2.0F, new CubeDeformation(-0.002F)).mirror(false),
+				PartPose.offsetAndRotation(0.0F, 6.0F, 0.0F, -0.6545F, 0.0F, 0.0F));
+
+		PartDefinition RightFrontLeg = Body.addOrReplaceChild("RightFrontLeg", CubeListBuilder.create().texOffs(95, 0)
+				.addBox(-3.0F, 1.0F, -3.0F, 6.0F, 18.0F, 6.0F, new CubeDeformation(0.0F)),
+				PartPose.offset(-8.0F, -19.0F, -16.0F));
+
+		PartDefinition LeftFrontLeg = Body.addOrReplaceChild("LeftFrontLeg", CubeListBuilder.create().texOffs(68, 0)
+				.addBox(-3.0F, 1.0F, -3.0F, 6.0F, 18.0F, 6.0F, new CubeDeformation(0.0F)),
+				PartPose.offset(8.0F, -19.0F, -16.0F));
+
+		PartDefinition LeftBackLeg = Body.addOrReplaceChild("LeftBackLeg", CubeListBuilder.create().texOffs(122, 0)
+				.addBox(-3.0F, 1.0F, -3.0F, 6.0F, 18.0F, 6.0F, new CubeDeformation(0.0F)),
+				PartPose.offset(8.0F, -19.0F, 16.0F));
+
+		PartDefinition RightBackLeg = Body.addOrReplaceChild("RightBackLeg", CubeListBuilder.create().texOffs(149, 0)
+				.addBox(-3.0F, 1.0F, -3.0F, 6.0F, 18.0F, 6.0F, new CubeDeformation(0.0F)),
+				PartPose.offset(-8.0F, -19.0F, 16.0F));
+
+		PartDefinition Chests = Body.addOrReplaceChild("Chests",
+				CubeListBuilder.create().texOffs(54, 38)
+						.addBox(11.0F, -37.0F, 9.5F, 2.0F, 8.0F, 8.0F, new CubeDeformation(0.0F)).texOffs(54, 38)
+						.mirror().addBox(-13.0F, -37.0F, 9.5F, 2.0F, 8.0F, 8.0F, new CubeDeformation(0.0F))
+						.mirror(false).texOffs(127, 112).mirror()
+						.addBox(10.2F, -42.475F, 11.5F, 1.0F, 6.0F, 4.0F, new CubeDeformation(0.0F)).mirror(false)
+						.texOffs(127, 112).addBox(-11.2F, -42.475F, 11.5F, 1.0F, 6.0F, 4.0F, new CubeDeformation(0.0F))
+						.texOffs(127, 105).addBox(-8.5F, -43.475F, 11.5F, 17.0F, 1.0F, 4.0F, new CubeDeformation(0.0F)),
+				PartPose.offset(0.0F, 0.0F, 0.0F));
 
 		PartDefinition HolderLeft = Chests.addOrReplaceChild("HolderLeft",
-				CubeListBuilder.create().texOffs(134, 89).addBox(-1.5F, -0.5F, -2.0F, 3.0F, 2.0F, 4.0F,
-						new CubeDeformation(0.0F)),
+				CubeListBuilder.create().texOffs(173, 104).addBox(-1.5F, -0.5F, -2.0F, 3.0F, 2.0F, 4.0F,
+						new CubeDeformation(-0.001F)),
 				PartPose.offsetAndRotation(9.6F, -42.5F, 13.5F, 0.0F, 0.0F, 0.3491F));
 
 		PartDefinition HolderRight = Chests.addOrReplaceChild("HolderRight",
-				CubeListBuilder.create().texOffs(134, 89).addBox(-1.5F, -0.5F, -2.0F, 3.0F, 2.0F, 4.0F,
-						new CubeDeformation(0.0F)),
+				CubeListBuilder.create().texOffs(173, 104).addBox(-1.5F, -0.5F, -2.0F, 3.0F, 2.0F, 4.0F,
+						new CubeDeformation(-0.001F)),
 				PartPose.offsetAndRotation(-9.6F, -42.5F, 13.5F, 0.0F, 0.0F, -0.3491F));
 
-		PartDefinition Saddle = partdefinition.addOrReplaceChild("Saddle",
-				CubeListBuilder.create().texOffs(114, 84)
-						.addBox(-6.5F, -44.0F, -7.0F, 13.0F, 1.0F, 12.0F, new CubeDeformation(0.0F)).texOffs(124, 96)
-						.addBox(-6.0F, -45.0F, 3.0F, 12.0F, 1.0F, 2.0F, new CubeDeformation(0.0F)).texOffs(136, 85)
-						.addBox(-11.2F, -42.475F, -2.0F, 1.0F, 10.0F, 2.0F, new CubeDeformation(0.0F)).texOffs(136, 85)
-						.addBox(10.2F, -42.475F, -2.0F, 1.0F, 10.0F, 2.0F, new CubeDeformation(0.0F)).texOffs(144, 90)
-						.addBox(-1.5F, -46.0F, -7.0F, 3.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)).texOffs(245, 43)
-						.addBox(10.4F, -32.5F, -2.5F, 1.0F, 1.0F, 3.0F, new CubeDeformation(0.0F)).texOffs(245, 43)
-						.addBox(-11.4F, -32.5F, -2.5F, 1.0F, 1.0F, 3.0F, new CubeDeformation(0.0F)).texOffs(245, 43)
-						.addBox(10.4F, -32.0F, -2.0F, 1.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)).texOffs(245, 43)
-						.addBox(-11.4F, -32.0F, -2.0F, 1.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)),
-				PartPose.offset(0.0F, 24.0F, 0.0F));
+		PartDefinition Saddle = Body.addOrReplaceChild("Saddle", CubeListBuilder.create().texOffs(127, 112)
+				.addBox(-6.5F, -44.0F, -7.0F, 13.0F, 1.0F, 12.0F, new CubeDeformation(0.0F)).texOffs(128, 100)
+				.addBox(-6.0F, -45.0F, 3.0F, 12.0F, 1.0F, 2.0F, new CubeDeformation(0.0F)).texOffs(181, 113)
+				.addBox(-11.2F, -42.475F, -2.0F, 1.0F, 10.0F, 2.0F, new CubeDeformation(0.0F)).texOffs(181, 113)
+				.mirror().addBox(10.2F, -42.475F, -2.0F, 1.0F, 10.0F, 2.0F, new CubeDeformation(0.0F)).mirror(false)
+				.texOffs(168, 117).addBox(-1.5F, -46.0F, -7.0F, 3.0F, 2.0F, 2.0F, new CubeDeformation(0.0F))
+				.texOffs(245, 43).addBox(10.4F, -32.5F, -2.5F, 1.0F, 1.0F, 3.0F, new CubeDeformation(0.0F))
+				.texOffs(245, 43).addBox(-11.4F, -32.5F, -2.5F, 1.0F, 1.0F, 3.0F, new CubeDeformation(0.0F))
+				.texOffs(245, 43).addBox(10.375F, -32.0F, -2.0F, 1.0F, 2.0F, 2.0F, new CubeDeformation(0.0F))
+				.texOffs(245, 43).addBox(-11.375F, -32.0F, -2.0F, 1.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)),
+				PartPose.offset(0.0F, 0.0F, 0.0F));
 
 		PartDefinition SaddleRight = Saddle.addOrReplaceChild("SaddleRight",
-				CubeListBuilder.create().texOffs(130, 84).addBox(-2.5492F, -0.0726F, -0.5F, 5.0F, 1.0F, 2.0F,
-						new CubeDeformation(0.0F)),
+				CubeListBuilder.create().texOffs(158, 100).addBox(-2.5492F, -0.0726F, -0.5F, 5.0F, 1.0F, 2.0F,
+						new CubeDeformation(-0.001F)),
 				PartPose.offsetAndRotation(-8.625F, -43.175F, -1.5F, 0.0F, 0.0F, -0.3054F));
 
 		PartDefinition SaddleLeft = Saddle.addOrReplaceChild("SaddleLeft",
-				CubeListBuilder.create().texOffs(130, 84).addBox(-2.5492F, -0.0726F, -0.5F, 5.0F, 1.0F, 2.0F,
-						new CubeDeformation(0.0F)),
+				CubeListBuilder.create().texOffs(158, 100).mirror()
+						.addBox(-2.5492F, -0.0726F, -0.5F, 5.0F, 1.0F, 2.0F, new CubeDeformation(-0.001F))
+						.mirror(false),
 				PartPose.offsetAndRotation(8.725F, -43.15F, -1.5F, 0.0F, 0.0F, 0.3054F));
 
 		return LayerDefinition.create(meshdefinition, 256, 128);
@@ -216,26 +203,18 @@ public class ElephantModel<T extends ElephantEntity> extends AdvancedEntityModel
 	@Override
 	public void renderToBuffer(PoseStack matrixStack, VertexConsumer buffer, int packedLight, int packedOverlay,
 			float red, float green, float blue, float alpha) {
-
 		if (this.young) {
 			matrixStack.scale(0.6F, 0.6F, 0.6F);
 			matrixStack.translate(0, 1, 0);
-		} else {
-			Tusks.render(matrixStack, buffer, packedLight, packedOverlay);
 		}
 		Body.render(matrixStack, buffer, packedLight, packedOverlay);
-		Chests.render(matrixStack, buffer, packedLight, packedOverlay);
-		Saddle.render(matrixStack, buffer, packedLight, packedOverlay);
 	}
 
 	@Override
 	public void setupAnim(ElephantEntity elephant, float limbSwing, float limbSwingAmount, float ageInTicks,
 			float netHeadYaw, float headPitch) {
-
 		this.Head.xRot = headPitch * 0.0174532925F;
-		this.Tusks.xRot = headPitch * 0.0174532925F;
 		this.Head.yRot = netHeadYaw * 0.0174532925F;
-		this.Tusks.yRot = netHeadYaw * 0.0174532925F;
 
 		this.walk(RightFrontLeg, LeftFrontLeg, RightBackLeg, LeftBackLeg, limbSwing, limbSwingAmount);
 
@@ -265,6 +244,7 @@ public class ElephantModel<T extends ElephantEntity> extends AdvancedEntityModel
 		}
 		this.Chests.visible = elephant.hasChest();
 		this.Saddle.visible = elephant.isSaddled();
+		this.Tusks.visible = !this.young;
 
 	}
 
--- src/main/java/com/tristankechlo/livingthings/client/renderer/ElephantRenderer.java
+++ src/main/java/com/tristankechlo/livingthings/client/renderer/ElephantRenderer.java
@@ -15,9 +15,7 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 public class ElephantRenderer extends MobRenderer<ElephantEntity, ElephantModel<ElephantEntity>> {
 
 	protected static final ResourceLocation TEXTURE = new ResourceLocation(LivingThings.MOD_ID,
-			"textures/entity/elephant/elephant.png");
-	protected static final ResourceLocation TEXTURE_SADDLED = new ResourceLocation(LivingThings.MOD_ID,
-			"textures/entity/elephant/elephant_saddled.png");
+			"textures/entity/elephant.png");
 
 	public ElephantRenderer(Context context) {
 		super(context, new ElephantModel<>(context.bakeLayer(ModelLayer.ELEPHANT)), 1.2F);
@@ -25,9 +23,7 @@ public class ElephantRenderer extends MobRenderer<ElephantEntity, ElephantModel<
 
 	@Override
 	public ResourceLocation getTextureLocation(ElephantEntity entity) {
-		if (entity.isTame() && !entity.isBaby()) {
-			return TEXTURE_SADDLED;
-		}
 		return TEXTURE;
 	}
+
 }
