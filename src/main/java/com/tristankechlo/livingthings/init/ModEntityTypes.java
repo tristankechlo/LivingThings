8c117ee9acb038d00d0d291f314a9fa0bc043a2e
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2022-03-04 14:44:49 +0100

    replace registry names

--- src/main/java/com/tristankechlo/livingthings/init/ModEntityTypes.java
+++ src/main/java/com/tristankechlo/livingthings/init/ModEntityTypes.java
@@ -31,49 +31,48 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class ModEntityTypes {
 
-	// TODO replace entity names
 	public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITIES, LivingThings.MOD_ID);
 
 	// create entity types
-	private static final EntityType<ElephantEntity> elephant = createStandardEntityType("elephant", ElephantEntity::new, MobCategory.CREATURE, 1.85F, 2.7F);
-	private static final EntityType<GiraffeEntity> giraffe = createStandardEntityType("giraffe", GiraffeEntity::new, MobCategory.CREATURE, 1.5F, 3.2F);
-	private static final EntityType<LionEntity> lion = createStandardEntityType("lion", LionEntity::new, MobCategory.CREATURE, 1.25F, 1.5F);
-	private static final EntityType<SharkEntity> shark = createStandardEntityType("shark", SharkEntity::new, MobCategory.WATER_CREATURE, 1.4F, 1.1F);
-	private static final EntityType<PenguinEntity> penguin = createStandardEntityType("penguin", PenguinEntity::new, MobCategory.CREATURE, 0.8F, 1.45F);
-	private static final EntityType<OstrichEntity> ostrich = createStandardEntityType("ostrich", OstrichEntity::new, MobCategory.CREATURE, 0.6F, 1.8F);
-	private static final EntityType<FlamingoEntity> flamingo = createStandardEntityType("flamingo", FlamingoEntity::new, MobCategory.CREATURE, 0.6F, 1.25F);
-	private static final EntityType<CrabEntity> crab = createStandardEntityType("crab", CrabEntity::new, MobCategory.CREATURE, 0.4F, 0.4F);
-	private static final EntityType<MantarayEntity> mantaray = createStandardEntityType("mantaray", MantarayEntity::new, MobCategory.WATER_AMBIENT, 0.75F, 0.45F);
-	private static final EntityType<RaccoonEntity> raccoon = createStandardEntityType("raccoon", RaccoonEntity::new, MobCategory.CREATURE, 0.5F, 0.75F);
-	private static final EntityType<OwlEntity> owl = createStandardEntityType("owl", OwlEntity::new, MobCategory.CREATURE, 0.5F, 0.99F);
-	private static final EntityType<AncientBlazeEntity> ancient_blaze = EntityType.Builder.of(AncientBlazeEntity::new, MobCategory.MONSTER).sized(0.7F, 2.99F).fireImmune().build(LivingThings.MOD_ID + ":ancient_blaze");
-	private static final EntityType<KoalaEntity> koala = createStandardEntityType("koala", KoalaEntity::new, MobCategory.CREATURE, 0.6F, 0.75F);
-	private static final EntityType<SnailEntity> snail = createStandardEntityType("snail", SnailEntity::new, MobCategory.CREATURE, 0.6F, 0.7F);
-	private static final EntityType<MonkeyEntity> monkey = createStandardEntityType("monkey", MonkeyEntity::new, MobCategory.CREATURE, 0.6F, 0.7F);
-	private static final EntityType<NetherKnightEntity> nether_knight = EntityType.Builder.of(NetherKnightEntity::new, MobCategory.MONSTER).sized(0.7F, 2.3F).fireImmune().build(LivingThings.MOD_ID + ":nether_knight");
-	private static final EntityType<ShroomieEntity> shroomie = createStandardEntityType("shroomie", ShroomieEntity::new, MobCategory.CREATURE, 0.5F, 0.99F);
-	public static final EntityType<SeahorseEntity> seahorse = createStandardEntityType("seahorse", SeahorseEntity::new, MobCategory.WATER_AMBIENT, 0.2F, 0.7F);
+	private static final EntityType<ElephantEntity> elephant = createStandardEntityType(Names.ELEPHANT, ElephantEntity::new, MobCategory.CREATURE, 1.85F, 2.7F);
+	private static final EntityType<GiraffeEntity> giraffe = createStandardEntityType(Names.GIRAFFE, GiraffeEntity::new, MobCategory.CREATURE, 1.5F, 3.2F);
+	private static final EntityType<LionEntity> lion = createStandardEntityType(Names.LION, LionEntity::new, MobCategory.CREATURE, 1.25F, 1.5F);
+	private static final EntityType<SharkEntity> shark = createStandardEntityType(Names.SHARK, SharkEntity::new, MobCategory.WATER_CREATURE, 1.4F, 1.1F);
+	private static final EntityType<PenguinEntity> penguin = createStandardEntityType(Names.PENGUIN, PenguinEntity::new, MobCategory.CREATURE, 0.8F, 1.45F);
+	private static final EntityType<OstrichEntity> ostrich = createStandardEntityType(Names.OSTRICH, OstrichEntity::new, MobCategory.CREATURE, 0.6F, 1.8F);
+	private static final EntityType<FlamingoEntity> flamingo = createStandardEntityType(Names.FLAMINGO, FlamingoEntity::new, MobCategory.CREATURE, 0.6F, 1.25F);
+	private static final EntityType<CrabEntity> crab = createStandardEntityType(Names.CRAB, CrabEntity::new, MobCategory.CREATURE, 0.4F, 0.4F);
+	private static final EntityType<MantarayEntity> mantaray = createStandardEntityType(Names.MANTARAY, MantarayEntity::new, MobCategory.WATER_AMBIENT, 0.75F, 0.45F);
+	private static final EntityType<RaccoonEntity> raccoon = createStandardEntityType(Names.RACCOON, RaccoonEntity::new, MobCategory.CREATURE, 0.5F, 0.75F);
+	private static final EntityType<OwlEntity> owl = createStandardEntityType(Names.OWL, OwlEntity::new, MobCategory.CREATURE, 0.5F, 0.99F);
+	private static final EntityType<AncientBlazeEntity> ancient_blaze = EntityType.Builder.of(AncientBlazeEntity::new, MobCategory.MONSTER).sized(0.7F, 2.99F).fireImmune().build(LivingThings.MOD_ID + ":" + Names.ANCIENT_BLAZE);
+	private static final EntityType<KoalaEntity> koala = createStandardEntityType(Names.KOALA, KoalaEntity::new, MobCategory.CREATURE, 0.6F, 0.75F);
+	private static final EntityType<SnailEntity> snail = createStandardEntityType(Names.SNAIL, SnailEntity::new, MobCategory.CREATURE, 0.6F, 0.7F);
+	private static final EntityType<MonkeyEntity> monkey = createStandardEntityType(Names.MONKEY, MonkeyEntity::new, MobCategory.CREATURE, 0.6F, 0.7F);
+	private static final EntityType<NetherKnightEntity> nether_knight = EntityType.Builder.of(NetherKnightEntity::new, MobCategory.MONSTER).sized(0.7F, 2.3F).fireImmune().build(LivingThings.MOD_ID + ":" + Names.NETHER_KNIGHT);
+	private static final EntityType<ShroomieEntity> shroomie = createStandardEntityType(Names.SHROOMIE, ShroomieEntity::new, MobCategory.CREATURE, 0.5F, 0.99F);
+	public static final EntityType<SeahorseEntity> seahorse = createStandardEntityType(Names.SEAHORSE, SeahorseEntity::new, MobCategory.WATER_AMBIENT, 0.2F, 0.7F);
 	private static final EntityType<BabyEnderDragonEntity> baby_ender_dragon = createStandardEntityType(Names.BABY_ENDER_DRAGON, BabyEnderDragonEntity::new, MobCategory.CREATURE, 0.75F, 0.75F);
 
 	// register entity types
-	public static final RegistryObject<EntityType<ElephantEntity>> ELEPHANT = ENTITY_TYPES.register("elephant", () -> elephant);
-	public static final RegistryObject<EntityType<GiraffeEntity>> GIRAFFE = ENTITY_TYPES.register("giraffe", () -> giraffe);
-	public static final RegistryObject<EntityType<LionEntity>> LION = ENTITY_TYPES.register("lion", () -> lion);
-	public static final RegistryObject<EntityType<SharkEntity>> SHARK = ENTITY_TYPES.register("shark", () -> shark);
-	public static final RegistryObject<EntityType<PenguinEntity>> PENGUIN = ENTITY_TYPES.register("penguin", () -> penguin);
-	public static final RegistryObject<EntityType<OstrichEntity>> OSTRICH = ENTITY_TYPES.register("ostrich", () -> ostrich);
-	public static final RegistryObject<EntityType<FlamingoEntity>> FLAMINGO = ENTITY_TYPES.register("flamingo", () -> flamingo);
-	public static final RegistryObject<EntityType<CrabEntity>> CRAB = ENTITY_TYPES.register("crab", () -> crab);
-	public static final RegistryObject<EntityType<MantarayEntity>> MANTARAY = ENTITY_TYPES.register("mantaray", () -> mantaray);
-	public static final RegistryObject<EntityType<RaccoonEntity>> RACCOON = ENTITY_TYPES.register("raccoon", () -> raccoon);
-	public static final RegistryObject<EntityType<OwlEntity>> OWL = ENTITY_TYPES.register("owl", () -> owl);
-	public static final RegistryObject<EntityType<AncientBlazeEntity>> ANCIENT_BLAZE = ENTITY_TYPES.register("ancient_blaze", () -> ancient_blaze);
-	public static final RegistryObject<EntityType<KoalaEntity>> KOALA = ENTITY_TYPES.register("koala", () -> koala);
-	public static final RegistryObject<EntityType<SnailEntity>> SNAIL = ENTITY_TYPES.register("snail", () -> snail);
-	public static final RegistryObject<EntityType<MonkeyEntity>> MONKEY = ENTITY_TYPES.register("monkey", () -> monkey);
-	public static final RegistryObject<EntityType<NetherKnightEntity>> NETHER_KNIGHT = ENTITY_TYPES.register("nether_knight", () -> nether_knight);
-	public static final RegistryObject<EntityType<ShroomieEntity>> SHROOMIE = ENTITY_TYPES.register("shroomie", () -> shroomie);
-	public static final RegistryObject<EntityType<SeahorseEntity>> SEAHORSE = ENTITY_TYPES.register("seahorse", () -> seahorse);
+	public static final RegistryObject<EntityType<ElephantEntity>> ELEPHANT = ENTITY_TYPES.register(Names.ELEPHANT, () -> elephant);
+	public static final RegistryObject<EntityType<GiraffeEntity>> GIRAFFE = ENTITY_TYPES.register(Names.GIRAFFE, () -> giraffe);
+	public static final RegistryObject<EntityType<LionEntity>> LION = ENTITY_TYPES.register(Names.LION, () -> lion);
+	public static final RegistryObject<EntityType<SharkEntity>> SHARK = ENTITY_TYPES.register(Names.SHARK, () -> shark);
+	public static final RegistryObject<EntityType<PenguinEntity>> PENGUIN = ENTITY_TYPES.register(Names.PENGUIN, () -> penguin);
+	public static final RegistryObject<EntityType<OstrichEntity>> OSTRICH = ENTITY_TYPES.register(Names.OSTRICH, () -> ostrich);
+	public static final RegistryObject<EntityType<FlamingoEntity>> FLAMINGO = ENTITY_TYPES.register(Names.FLAMINGO, () -> flamingo);
+	public static final RegistryObject<EntityType<CrabEntity>> CRAB = ENTITY_TYPES.register(Names.CRAB, () -> crab);
+	public static final RegistryObject<EntityType<MantarayEntity>> MANTARAY = ENTITY_TYPES.register(Names.MANTARAY, () -> mantaray);
+	public static final RegistryObject<EntityType<RaccoonEntity>> RACCOON = ENTITY_TYPES.register(Names.RACCOON, () -> raccoon);
+	public static final RegistryObject<EntityType<OwlEntity>> OWL = ENTITY_TYPES.register(Names.OWL, () -> owl);
+	public static final RegistryObject<EntityType<AncientBlazeEntity>> ANCIENT_BLAZE = ENTITY_TYPES.register(Names.ANCIENT_BLAZE, () -> ancient_blaze);
+	public static final RegistryObject<EntityType<KoalaEntity>> KOALA = ENTITY_TYPES.register(Names.KOALA, () -> koala);
+	public static final RegistryObject<EntityType<SnailEntity>> SNAIL = ENTITY_TYPES.register(Names.SNAIL, () -> snail);
+	public static final RegistryObject<EntityType<MonkeyEntity>> MONKEY = ENTITY_TYPES.register(Names.MONKEY, () -> monkey);
+	public static final RegistryObject<EntityType<NetherKnightEntity>> NETHER_KNIGHT = ENTITY_TYPES.register(Names.NETHER_KNIGHT, () -> nether_knight);
+	public static final RegistryObject<EntityType<ShroomieEntity>> SHROOMIE = ENTITY_TYPES.register(Names.SHROOMIE, () -> shroomie);
+	public static final RegistryObject<EntityType<SeahorseEntity>> SEAHORSE = ENTITY_TYPES.register(Names.SEAHORSE, () -> seahorse);
 	public static final RegistryObject<EntityType<BabyEnderDragonEntity>> BABY_ENDER_DRAGON = ENTITY_TYPES.register(Names.BABY_ENDER_DRAGON, () -> baby_ender_dragon);
 
 	// create standard entity type
--- src/main/java/com/tristankechlo/livingthings/init/ModItems.java
+++ src/main/java/com/tristankechlo/livingthings/init/ModItems.java
@@ -1,5 +1,7 @@
 package com.tristankechlo.livingthings.init;
 
+import java.util.function.Supplier;
+
 import com.tristankechlo.livingthings.LivingThings;
 import com.tristankechlo.livingthings.items.LexiconItem;
 import com.tristankechlo.livingthings.items.ModArmorItem;
@@ -7,7 +9,9 @@ import com.tristankechlo.livingthings.items.OstrichEggItem;
 import com.tristankechlo.livingthings.misc.Names;
 
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Item.Properties;
 import net.minecraft.world.item.MobBucketItem;
@@ -42,25 +46,28 @@ public final class ModItems {
 	public static final RegistryObject<Item> SEAHORSE_BUCKET = ITEMS.register("seahorse_bucket", () -> new MobBucketItem(ModEntityTypes.seahorse, Fluids.WATER, SoundEvents.BUCKET_EMPTY_FISH, new Properties().tab(ModItemGroups.GENERAL).stacksTo(1)));
 
 	// register spawn eggs
-	// TODO replace item names
-	public static final RegistryObject<Item> ELEPHANT_SPAWN_EGG = ITEMS.register("elephant_spawn_egg", () -> new ForgeSpawnEggItem(ModEntityTypes.ELEPHANT, 0x000000, 0x4e4e4e, SPAWN_EGG_PROPERTIES));
-	public static final RegistryObject<Item> GIRAFFE_SPAWN_EGG = ITEMS.register("giraffe_spawn_egg", () -> new ForgeSpawnEggItem(ModEntityTypes.GIRAFFE, 0xebb26c, 0x785f40, SPAWN_EGG_PROPERTIES));
-	public static final RegistryObject<Item> LION_SPAWN_EGG = ITEMS.register("lion_spawn_egg", () -> new ForgeSpawnEggItem(ModEntityTypes.LION, 0xebb26c, 0xFFFFFF, SPAWN_EGG_PROPERTIES));
-	public static final RegistryObject<Item> SHARK_SPAWN_EGG = ITEMS.register("shark_spawn_egg", () -> new ForgeSpawnEggItem(ModEntityTypes.SHARK, 0x707187, 0x595a6b, SPAWN_EGG_PROPERTIES));
-	public static final RegistryObject<Item> PENGUIN_SPAWN_EGG = ITEMS.register("penguin_spawn_egg", () -> new ForgeSpawnEggItem(ModEntityTypes.PENGUIN, 0x000000, 0xFFFFFF, SPAWN_EGG_PROPERTIES));
-	public static final RegistryObject<Item> OSTRICH_SPAWN_EGG = ITEMS.register("ostrich_spawn_egg", () -> new ForgeSpawnEggItem(ModEntityTypes.OSTRICH, 0x130d08, 0xa56f5b, SPAWN_EGG_PROPERTIES));
-	public static final RegistryObject<Item> FLAMINGO_SPAWN_EGG = ITEMS.register("flamingo_spawn_egg", () -> new ForgeSpawnEggItem(ModEntityTypes.FLAMINGO, 0xf38989, 0x2d0404, SPAWN_EGG_PROPERTIES));
-	public static final RegistryObject<Item> CRAB_SPAWN_EGG = ITEMS.register("crab_spawn_egg", () -> new ForgeSpawnEggItem(ModEntityTypes.CRAB, 0xeb4034, 0x73706f, SPAWN_EGG_PROPERTIES));
-	public static final RegistryObject<Item> MANTARAY_SPAWN_EGG = ITEMS.register("mantaray_spawn_egg", () -> new ForgeSpawnEggItem(ModEntityTypes.MANTARAY, 0x000896, 0x595a6b, SPAWN_EGG_PROPERTIES));
-	public static final RegistryObject<Item> RACCOON_SPAWN_EGG = ITEMS.register("raccoon_spawn_egg", () -> new ForgeSpawnEggItem(ModEntityTypes.RACCOON, 0x6e6e6e, 0x000000, SPAWN_EGG_PROPERTIES));
-	public static final RegistryObject<Item> OWL_SPAWN_EGG = ITEMS.register("owl_spawn_egg", () -> new ForgeSpawnEggItem(ModEntityTypes.OWL, 0xedd7d5, 0x6e3834, SPAWN_EGG_PROPERTIES));
-	public static final RegistryObject<Item> ANCIENT_BLAZE_SPAWN_EGG = ITEMS.register("ancient_blaze_spawn_egg", () -> new ForgeSpawnEggItem(ModEntityTypes.ANCIENT_BLAZE, 0xF6B200, 0xFFF87D, SPAWN_EGG_PROPERTIES));
-	public static final RegistryObject<Item> KOALA_SPAWN_EGG = ITEMS.register("koala_spawn_egg", () -> new ForgeSpawnEggItem(ModEntityTypes.KOALA, 0x565050, 0x8f8686, SPAWN_EGG_PROPERTIES));
-	public static final RegistryObject<Item> SNAIL_SPAWN_EGG = ITEMS.register("snail_spawn_egg", () -> new ForgeSpawnEggItem(ModEntityTypes.SNAIL, 0x2206464, 0x53588, SPAWN_EGG_PROPERTIES));
-	public static final RegistryObject<Item> MONKEY_SPAWN_EGG = ITEMS.register("monkey_spawn_egg", () -> new ForgeSpawnEggItem(ModEntityTypes.MONKEY, 10051392, 7555121, SPAWN_EGG_PROPERTIES));
-	public static final RegistryObject<Item> NETHER_KNIGHT_SPAWN_EGG = ITEMS.register("nether_knight_spawn_egg", () -> new ForgeSpawnEggItem(ModEntityTypes.NETHER_KNIGHT, 0x181a1c, 0xa32aa1, SPAWN_EGG_PROPERTIES));
-	public static final RegistryObject<Item> SHROOMIE_SPAWN_EGG = ITEMS.register("shroomie_spawn_egg", () -> new ForgeSpawnEggItem(ModEntityTypes.SHROOMIE, 0xb8968d, 0xdb380f, SPAWN_EGG_PROPERTIES));
-	public static final RegistryObject<Item> SEAHORSE_SPAWN_EGG = ITEMS.register("seahorse_spawn_egg", () -> new ForgeSpawnEggItem(ModEntityTypes.SEAHORSE, 0x22ff22, 0xdb380f, SPAWN_EGG_PROPERTIES));
-	public static final RegistryObject<Item> BABY_ENDER_DRAGON_SPAWN_EGG = ITEMS.register(Names.BABY_ENDER_DRAGON + "_spawn_egg", () -> new ForgeSpawnEggItem(ModEntityTypes.BABY_ENDER_DRAGON, 0x201e24, 0xff59cd, SPAWN_EGG_PROPERTIES));
-	
+	public static final RegistryObject<Item> ELEPHANT_SPAWN_EGG = registerSpawnEgg(Names.ELEPHANT, ModEntityTypes.ELEPHANT, 0x000000, 0x4e4e4e);
+	public static final RegistryObject<Item> GIRAFFE_SPAWN_EGG = registerSpawnEgg(Names.GIRAFFE, ModEntityTypes.GIRAFFE, 0xebb26c, 0x785f40);
+	public static final RegistryObject<Item> LION_SPAWN_EGG = registerSpawnEgg(Names.LION, ModEntityTypes.LION, 0xebb26c, 0xFFFFFF);
+	public static final RegistryObject<Item> SHARK_SPAWN_EGG = registerSpawnEgg(Names.SHARK, ModEntityTypes.SHARK, 0x707187, 0x595a6b);
+	public static final RegistryObject<Item> PENGUIN_SPAWN_EGG = registerSpawnEgg(Names.PENGUIN, ModEntityTypes.PENGUIN, 0x000000, 0xFFFFFF);
+	public static final RegistryObject<Item> OSTRICH_SPAWN_EGG = registerSpawnEgg(Names.OSTRICH, ModEntityTypes.OSTRICH, 0x130d08, 0xa56f5b);
+	public static final RegistryObject<Item> FLAMINGO_SPAWN_EGG = registerSpawnEgg(Names.FLAMINGO, ModEntityTypes.FLAMINGO, 0xf38989, 0x2d0404);
+	public static final RegistryObject<Item> CRAB_SPAWN_EGG = registerSpawnEgg(Names.CRAB, ModEntityTypes.CRAB, 0xeb4034, 0x73706f);
+	public static final RegistryObject<Item> MANTARAY_SPAWN_EGG = registerSpawnEgg(Names.MANTARAY, ModEntityTypes.MANTARAY, 0x000896, 0x595a6b);
+	public static final RegistryObject<Item> RACCOON_SPAWN_EGG = registerSpawnEgg(Names.RACCOON, ModEntityTypes.RACCOON, 0x6e6e6e, 0x000000);
+	public static final RegistryObject<Item> OWL_SPAWN_EGG = registerSpawnEgg(Names.OWL, ModEntityTypes.OWL, 0xedd7d5, 0x6e3834);
+	public static final RegistryObject<Item> ANCIENT_BLAZE_SPAWN_EGG = registerSpawnEgg(Names.ANCIENT_BLAZE, ModEntityTypes.ANCIENT_BLAZE, 0xF6B200, 0xFFF87D);
+	public static final RegistryObject<Item> KOALA_SPAWN_EGG = registerSpawnEgg(Names.KOALA, ModEntityTypes.KOALA, 0x565050, 0x8f8686);
+	public static final RegistryObject<Item> SNAIL_SPAWN_EGG = registerSpawnEgg(Names.SNAIL, ModEntityTypes.SNAIL, 0x2206464, 0x53588);
+	public static final RegistryObject<Item> MONKEY_SPAWN_EGG = registerSpawnEgg(Names.MONKEY, ModEntityTypes.MONKEY, 10051392, 7555121);
+	public static final RegistryObject<Item> NETHER_KNIGHT_SPAWN_EGG = registerSpawnEgg(Names.NETHER_KNIGHT, ModEntityTypes.NETHER_KNIGHT, 0x181a1c, 0xa32aa1);
+	public static final RegistryObject<Item> SHROOMIE_SPAWN_EGG = registerSpawnEgg(Names.SHROOMIE, ModEntityTypes.SHROOMIE, 0xb8968d, 0xdb380f);
+	public static final RegistryObject<Item> SEAHORSE_SPAWN_EGG = registerSpawnEgg(Names.SEAHORSE, ModEntityTypes.SEAHORSE, 0x22ff22, 0xdb380f);
+	public static final RegistryObject<Item> BABY_ENDER_DRAGON_SPAWN_EGG = registerSpawnEgg(Names.BABY_ENDER_DRAGON, ModEntityTypes.BABY_ENDER_DRAGON, 0x201e24, 0xff59cd);
+
+	private static RegistryObject<Item> registerSpawnEgg(String entityName, Supplier<? extends EntityType<? extends Mob>> type, int backgroundColor, int highlightColor) {
+		return ITEMS.register(entityName + "_spawn_egg", () -> new ForgeSpawnEggItem(type, backgroundColor, highlightColor, SPAWN_EGG_PROPERTIES));
+	}
+
 }
