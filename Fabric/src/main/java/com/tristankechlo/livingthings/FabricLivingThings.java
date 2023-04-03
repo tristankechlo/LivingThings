5b7bb0810198eddce347f283d78dc3ede47fcd9a
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2023-04-03 15:32:35 +0200

    add ancient blaze and baby ender dragon spawning

--- Fabric/src/main/java/com/tristankechlo/livingthings/FabricLivingThings.java
+++ Fabric/src/main/java/com/tristankechlo/livingthings/FabricLivingThings.java
@@ -2,9 +2,17 @@ package com.tristankechlo.livingthings;
 
 import com.tristankechlo.livingthings.commands.LivingThingsCommand;
 import com.tristankechlo.livingthings.config.ConfigManager;
+import com.tristankechlo.livingthings.events.BlockEvents;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.state.BlockState;
 
 public final class FabricLivingThings implements ModInitializer {
 
@@ -18,6 +26,20 @@ public final class FabricLivingThings implements ModInitializer {
         // register commands
         CommandRegistrationCallback.EVENT.register((dispatcher, dedicated, environment) -> {
             LivingThingsCommand.register(dispatcher);
+        });
+
+        PlayerBlockBreakEvents.AFTER.register((world, player, pos, state, blockEntity) -> {
+            BlockEvents.onBlockBreak(world, player, pos, state);
+        });
+
+        UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
+            Item item = player.getMainHandItem().getItem();
+            if (item instanceof BlockItem) {
+                BlockState placedBlock = ((BlockItem) item).getBlock().defaultBlockState();
+                BlockPos pos = hitResult.getBlockPos().relative(hitResult.getDirection());
+                return BlockEvents.onBlockPlace(world, player, pos, placedBlock);
+            }
+            return InteractionResult.PASS;
         });
     }
 
--- Forge/src/main/java/com/tristankechlo/livingthings/ForgeLivingThings.java
+++ Forge/src/main/java/com/tristankechlo/livingthings/ForgeLivingThings.java
@@ -2,10 +2,14 @@ package com.tristankechlo.livingthings;
 
 import com.tristankechlo.livingthings.commands.LivingThingsCommand;
 import com.tristankechlo.livingthings.config.ConfigManager;
+import com.tristankechlo.livingthings.events.BlockEvents;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.event.entity.SpawnPlacementRegisterEvent;
+import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -22,6 +26,8 @@ public final class ForgeLivingThings {
         modbus.addListener(this::onSpawnPlacementsRegister);
 
         MinecraftForge.EVENT_BUS.addListener(this::registerCommands);
+        MinecraftForge.EVENT_BUS.addListener(this::onBlockBreak);
+        MinecraftForge.EVENT_BUS.addListener(this::onBlockPlace);
         MinecraftForge.EVENT_BUS.register(this);
     }
 
@@ -29,12 +35,23 @@ public final class ForgeLivingThings {
         LivingThingsCommand.register(event.getDispatcher());
     }
 
-    public void onAttributeRegister(final EntityAttributeCreationEvent event) {
+    private void onAttributeRegister(final EntityAttributeCreationEvent event) {
         LivingThings.registerMobAttributes((entityType, builder) -> event.put(entityType, builder.build()));
     }
 
-    public void onSpawnPlacementsRegister(final SpawnPlacementRegisterEvent event) {
+    private void onSpawnPlacementsRegister(final SpawnPlacementRegisterEvent event) {
         LivingThings.registerSpawnPlacements();
+    }
+
+    private void onBlockBreak(final BlockEvent.BreakEvent event) {
+        BlockEvents.onBlockBreak(event.getLevel(), event.getPlayer(), event.getPos(), event.getState());
+    }
+
+    private void onBlockPlace(final BlockEvent.EntityPlaceEvent event) {
+        Entity entity = event.getEntity();
+        if (entity instanceof Player) {
+            BlockEvents.onBlockPlace(event.getLevel(), (Player) entity, event.getPos(), event.getPlacedBlock());
+        }
     }
 
 }
