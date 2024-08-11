c91659fddffb4cefb527d0de1ba9e44a80f7a9e4
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2024-08-11 16:45:46 +0200

    baby ender dragon now immune to own area effect clouds

--- common/src/main/java/com/tristankechlo/livingthings/entity/BabyEnderDragonEntity.java
+++ common/src/main/java/com/tristankechlo/livingthings/entity/BabyEnderDragonEntity.java
@@ -339,6 +339,10 @@ public class BabyEnderDragonEntity extends TamableAnimal implements NeutralMob, 
             this.setOrderedToSit(false);
             this.setTarget((LivingEntity) target);
         }
+        if (source.getDirectEntity() instanceof AreaEffectCloud) {
+            // can not be damaged by own AreaEffectCloud
+            return this == source.getEntity();
+        }
         return super.hurt(source, damage);
     }
 
