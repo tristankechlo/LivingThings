71a6914da638e5b0a7ddd34e75e9dd377879bf97
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2024-12-13 22:03:09 +0100

    small fixes

--- common/src/main/java/com/tristankechlo/livingthings/init/ModItems.java
+++ common/src/main/java/com/tristankechlo/livingthings/init/ModItems.java
@@ -59,26 +59,26 @@ public final class ModItems {
     public static final RegistryObject<Item> SEAHORSE_BUCKET = registerItem("seahorse_bucket", (p) -> IPlatformHelper.INSTANCE.createMobBucketItem(ModEntityTypes.SEAHORSE, Fluids.WATER, SoundEvents.BUCKET_EMPTY_FISH, p.stacksTo(1)));
 
     // register spawn eggs
-    public static final RegistryObject<Item> ELEPHANT_SPAWN_EGG = registerSpawnEgg("elephant_spawn_egg", ModEntityTypes.ELEPHANT::get, 0x000000, 0x4e4e4e);
-    public static final RegistryObject<Item> GIRAFFE_SPAWN_EGG = registerSpawnEgg("giraffe_spawn_egg", ModEntityTypes.GIRAFFE::get, 0xebb26c, 0x785f40);
-    public static final RegistryObject<Item> LION_SPAWN_EGG = registerSpawnEgg("lion_spawn_egg", ModEntityTypes.LION::get, 0xebb26c, 0xFFFFFF);
-    public static final RegistryObject<Item> SHARK_SPAWN_EGG = registerSpawnEgg("shark_spawn_egg", ModEntityTypes.SHARK::get, 0x707187, 0x595a6b);
-    public static final RegistryObject<Item> PENGUIN_SPAWN_EGG = registerSpawnEgg("penguin_spawn_egg", ModEntityTypes.PENGUIN::get, 0x000000, 0xFFFFFF);
-    public static final RegistryObject<Item> OSTRICH_SPAWN_EGG = registerSpawnEgg("ostrich_spawn_egg", ModEntityTypes.OSTRICH::get, 0x130d08, 0xa56f5b);
-    public static final RegistryObject<Item> FLAMINGO_SPAWN_EGG = registerSpawnEgg("flamingo_spawn_egg", ModEntityTypes.FLAMINGO::get, 0xf38989, 0x2d0404);
-    public static final RegistryObject<Item> CRAB_SPAWN_EGG = registerSpawnEgg("crab_spawn_egg", ModEntityTypes.CRAB::get, 0xeb4034, 0x73706f);
-    public static final RegistryObject<Item> MANTARAY_SPAWN_EGG = registerSpawnEgg("mantaray_spawn_egg", ModEntityTypes.MANTARAY::get, 0x000896, 0x595a6b);
-    public static final RegistryObject<Item> RACCOON_SPAWN_EGG = registerSpawnEgg("raccoon_spawn_egg", ModEntityTypes.RACCOON::get, 0x6e6e6e, 0x000000);
-    public static final RegistryObject<Item> OWL_SPAWN_EGG = registerSpawnEgg("owl_spawn_egg", ModEntityTypes.OWL::get, 0xedd7d5, 0x6e3834);
-    public static final RegistryObject<Item> ANCIENT_BLAZE_SPAWN_EGG = registerSpawnEgg("ancient_blaze_spawn_egg", ModEntityTypes.ANCIENT_BLAZE::get, 0xF6B200, 0xFFF87D);
-    public static final RegistryObject<Item> KOALA_SPAWN_EGG = registerSpawnEgg("koala_spawn_egg", ModEntityTypes.KOALA::get, 0x565050, 0x8f8686);
-    public static final RegistryObject<Item> SNAIL_SPAWN_EGG = registerSpawnEgg("snail_spawn_egg", ModEntityTypes.SNAIL::get, 0x2206464, 0x53588);
-    public static final RegistryObject<Item> MONKEY_SPAWN_EGG = registerSpawnEgg("monkey_spawn_egg", ModEntityTypes.MONKEY::get, 10051392, 7555121);
-    public static final RegistryObject<Item> NETHER_KNIGHT_SPAWN_EGG = registerSpawnEgg("nether_knight_spawn_egg", ModEntityTypes.NETHER_KNIGHT::get, 0x181a1c, 0xa32aa1);
-    public static final RegistryObject<Item> SHROOMIE_SPAWN_EGG = registerSpawnEgg("shroomie_spawn_egg", ModEntityTypes.SHROOMIE::get, 0xb8968d, 0xdb380f);
-    public static final RegistryObject<Item> SEAHORSE_SPAWN_EGG = registerSpawnEgg("seahorse_spawn_egg", ModEntityTypes.SEAHORSE::get, 0x22ff22, 0xdb380f);
-    public static final RegistryObject<Item> BABY_ENDER_DRAGON_SPAWN_EGG = registerSpawnEgg("baby_ender_dragon_spawn_egg", ModEntityTypes.BABY_ENDER_DRAGON::get, 0x201e24, 0xff59cd);
-    public static final RegistryObject<Item> PEACOCK_SPAWN_EGG = registerSpawnEgg("peacock_spawn_egg", ModEntityTypes.PEACOCK::get, 0x117fc7, 0x86d43b);
+    public static final RegistryObject<Item> ELEPHANT_SPAWN_EGG = registerSpawnEgg("elephant_spawn_egg", ModEntityTypes.ELEPHANT::get);
+    public static final RegistryObject<Item> GIRAFFE_SPAWN_EGG = registerSpawnEgg("giraffe_spawn_egg", ModEntityTypes.GIRAFFE::get);
+    public static final RegistryObject<Item> LION_SPAWN_EGG = registerSpawnEgg("lion_spawn_egg", ModEntityTypes.LION::get);
+    public static final RegistryObject<Item> SHARK_SPAWN_EGG = registerSpawnEgg("shark_spawn_egg", ModEntityTypes.SHARK::get);
+    public static final RegistryObject<Item> PENGUIN_SPAWN_EGG = registerSpawnEgg("penguin_spawn_egg", ModEntityTypes.PENGUIN::get);
+    public static final RegistryObject<Item> OSTRICH_SPAWN_EGG = registerSpawnEgg("ostrich_spawn_egg", ModEntityTypes.OSTRICH::get);
+    public static final RegistryObject<Item> FLAMINGO_SPAWN_EGG = registerSpawnEgg("flamingo_spawn_egg", ModEntityTypes.FLAMINGO::get);
+    public static final RegistryObject<Item> CRAB_SPAWN_EGG = registerSpawnEgg("crab_spawn_egg", ModEntityTypes.CRAB::get);
+    public static final RegistryObject<Item> MANTARAY_SPAWN_EGG = registerSpawnEgg("mantaray_spawn_egg", ModEntityTypes.MANTARAY::get);
+    public static final RegistryObject<Item> RACCOON_SPAWN_EGG = registerSpawnEgg("raccoon_spawn_egg", ModEntityTypes.RACCOON::get);
+    public static final RegistryObject<Item> OWL_SPAWN_EGG = registerSpawnEgg("owl_spawn_egg", ModEntityTypes.OWL::get);
+    public static final RegistryObject<Item> ANCIENT_BLAZE_SPAWN_EGG = registerSpawnEgg("ancient_blaze_spawn_egg", ModEntityTypes.ANCIENT_BLAZE::get);
+    public static final RegistryObject<Item> KOALA_SPAWN_EGG = registerSpawnEgg("koala_spawn_egg", ModEntityTypes.KOALA::get);
+    public static final RegistryObject<Item> SNAIL_SPAWN_EGG = registerSpawnEgg("snail_spawn_egg", ModEntityTypes.SNAIL::get);
+    public static final RegistryObject<Item> MONKEY_SPAWN_EGG = registerSpawnEgg("monkey_spawn_egg", ModEntityTypes.MONKEY::get);
+    public static final RegistryObject<Item> NETHER_KNIGHT_SPAWN_EGG = registerSpawnEgg("nether_knight_spawn_egg", ModEntityTypes.NETHER_KNIGHT::get);
+    public static final RegistryObject<Item> SHROOMIE_SPAWN_EGG = registerSpawnEgg("shroomie_spawn_egg", ModEntityTypes.SHROOMIE::get);
+    public static final RegistryObject<Item> SEAHORSE_SPAWN_EGG = registerSpawnEgg("seahorse_spawn_egg", ModEntityTypes.SEAHORSE::get);
+    public static final RegistryObject<Item> BABY_ENDER_DRAGON_SPAWN_EGG = registerSpawnEgg("baby_ender_dragon_spawn_egg", ModEntityTypes.BABY_ENDER_DRAGON::get);
+    public static final RegistryObject<Item> PEACOCK_SPAWN_EGG = registerSpawnEgg("peacock_spawn_egg", ModEntityTypes.PEACOCK::get);
 
     private static RegistryObject<Item> registerItem(String name, Function<Item.Properties, Item> item) {
         Item.Properties p = new Item.Properties().setId(RegistryHelper.itemId(name)).useItemDescriptionPrefix();
@@ -118,7 +118,7 @@ public final class ModItems {
         return registerItem(name, (p) -> new Item(p.food(food)));
     }
 
-    private static RegistryObject<Item> registerSpawnEgg(String name, Supplier<EntityType<? extends Mob>> type, int primaryColor, int secondaryColor) {
+    private static RegistryObject<Item> registerSpawnEgg(String name, Supplier<EntityType<? extends Mob>> type) {
         Item.Properties p = new Item.Properties().setId(RegistryHelper.itemId(name)).useItemDescriptionPrefix();
         RegistryObject<Item> registeredItem = ITEMS.register(name, () -> new SpawnEggItem(type.get(), p));
         SPAWN_EGGS.add(registeredItem);
